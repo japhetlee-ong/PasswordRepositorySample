9c787e3e42b7c9c2b1722aadc4519c12c353d46f
Japhet Lee Ong <japhetlee.dev@gmail.com>
2023-05-30 18:50:10 +0800

    Test push for VS

--- WebApplication1/Controllers/DashboardController.cs
+++ WebApplication1/Controllers/DashboardController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -47,9 +48,36 @@ namespace WebApplication1.Controllers
         }
 
         //[HttpPost]
-        //public ActionResult Index(PasswordModel)
+        //public ActionResult Index(PasswordModel _MODEL)
         //{
+        //    var userId = (Int32)Session["user_id"];
 
+        //    var ePassword = Helpers.Encryption.Encrypt(_MODEL.SITE_PASSWORD);
+
+        //    var data = new TBL_USER_PASSWORDS()
+        //    {
+        //        SITE_NAME = _MODEL.SITE_NAME,
+        //        SITE_PASSWORD = ePassword,
+        //        DATE_CREATED = DateTime.Now,
+        //        IS_DELETED = false,
+        //        DATE_MODIFIED = DateTime.Now,
+        //        USER_ID = userId
+        //    };
+
+        //    using (PassRepoDbEntities entities = new PassRepoDbEntities())
+        //    {
+        //        entities.TBL_USER_PASSWORDS.Add(data);
+
+        //        if (entities.SaveChanges() >= 1)
+        //        {
+        //            //RETURN HERE
+        //        }
+        //        else
+        //        {
+        //            //RETURN HERE
+        //        }
+
+        //    }
         //}
 
     }
--- WebApplication1/Controllers/RegisterController.cs
+++ WebApplication1/Controllers/RegisterController.cs
@@ -10,6 +10,7 @@ namespace WebApplication1.Controllers
     public class RegisterController : Controller
     {
         // GET: Register
+        
         public ActionResult Index()
         {
             return View();
